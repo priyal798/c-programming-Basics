bf115dae3e4d460fcfe4b484198c2a5869651a06
priyal798 <chaudharypriyal202211@gmail.com>
2026-08-29 11:14:30 +0530

    Create average_of_3_numbers.c

---
+++ average_of_3_numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // to print the average of three number
+    float a,b,c;
+    printf("enter three number ");
+    scanf("%f %f %f",&a,&b,&c);
+    printf("average of three number=%f",(a+b+c)/3);
+
+    return 0;
+}
