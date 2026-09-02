8bd4fd0c72e92fc13f39b33a69af212283336eb8
priyal798 <chaudharypriyal202211@gmail.com>
2026-09-02 18:00:35 +0530

    Create use_post_increment_operator.c

---
+++ use_post_increment_operator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // use of increment operator
+    //  -pre increment(++i)  -post increment(i++)  
+    int i = 1;
+     printf("%d\n",i++);
+     printf("%d\n",i);        
+
+    return 0;
+}
