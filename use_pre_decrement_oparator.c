74669ebc2cba80b42b5629acf173a299d5464b75
priyal798 <chaudharypriyal202211@gmail.com>
2026-09-02 18:09:18 +0530

    Add example of pre-decrement operator in C
    Demonstrate the use of pre-decrement operator in C.

---
+++ use_pre_decrement_oparator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // decrement operator
+    // types{ 1.pre decrement(--i)} {2.post decrement(i--)}
+    int i = 1;
+    printf("%d\n",--i);
+    printf("%d\n",i);
+
+    return 0;
+}
